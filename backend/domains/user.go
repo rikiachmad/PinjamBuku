2a012863e8832420827d804af607643f3d8235af
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2022-06-14 23:12:28 +0700

    feat: add entity for user

--- backend/domains/user.go
+++ backend/domains/user.go
@@ -1,1 +1,12 @@
-package domain
+package users
+
+type User struct {
+	ID          int64  `db:"id"`
+	Fullname    string `db:"fullname"`
+	Address     string `db:"address"`
+	Email       string `db:"email"`
+	Password    string `db:"password"`
+	PhoneNumber string `db:"phone_number"`
+	Verified    string `db:"verified"`
+	Role        string `db:"role"`
+}
