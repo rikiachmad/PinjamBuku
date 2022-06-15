97547b8c34e9145886e2a20ab881ab6575746b58
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2022-06-15 17:32:10 +0700

    feat: add router and orchestrate all layer in main script

---
+++ backend/app/routes/auth.go
@@ -0,0 +1,36 @@
+package routes
+
+import (
+	"github.com/rg-km/final-project-engineering-16/backend/app/handler"
+
+	"github.com/gin-gonic/gin"
+)
+
+type RequestHandler struct {
+	Gin *gin.Engine
+}
+
+// AuthRoutes struct
+type AuthRoutes struct {
+	handler        RequestHandler
+	authController handler.AuthController
+}
+
+// Setup user routes
+func (s AuthRoutes) Setup() {
+	auth := s.handler.Gin.Group("/auth")
+	{
+		auth.POST("/login", s.authController.SignIn)
+	}
+}
+
+// NewAuthRoutes creates new user controller
+func NewAuthRoutes(
+	handler RequestHandler,
+	authController handler.AuthController,
+) AuthRoutes {
+	return AuthRoutes{
+		handler:        handler,
+		authController: authController,
+	}
+}
