d1daa72c6a8377e70ba44f52e92c6e0684904285
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2022-06-16 21:55:10 +0700

    feat: add response struct to return created user instance

--- backend/app/presenter/auth.go
+++ backend/app/presenter/auth.go
@@ -4,16 +4,40 @@ import (
 	"github.com/rg-km/final-project-engineering-16/backend/domains"
 )
 
-type Auth struct {
+type Login struct {
 	Email          string     `json:"email"`
 	Fullname       string     `json:"fullname"`
 	Token          string     `json:"token,omitempty"`
 }
 
-func AuthFromDomain(u domains.User) Auth {
-	return Auth{
+func LoginFromDomain(u domains.User) Login {
+	return Login{
 		Email:          u.Email,
 		Fullname:       u.Fullname,
 		Token:          u.Token,
+	}
+}
+
+type CreateUser struct {
+	Email          string     `json:"email"`
+	Fullname       string     `json:"fullname"`
+	Address		string     `json:"address"`
+	PhoneNumber	string     `json:"phone_number"`
+	Role		string		`json:"role"`
+	Photo		string     `json:"photo"`
+	CreatedAt	string     `json:"created_at"`
+	UpdatedAt	string     `json:"updated_at"`
+}
+
+func CreateUserFromDomain(u domains.User) CreateUser {
+	return CreateUser{
+		Email:          u.Email,
+		Fullname:       u.Fullname,
+		Address:		u.Address,
+		PhoneNumber:	u.PhoneNumber,
+		Role:			u.Role,
+		Photo:			u.Photo,
+		CreatedAt:		u.CreatedAt,
+		UpdatedAt:		u.UpdatedAt,
 	}
 }
