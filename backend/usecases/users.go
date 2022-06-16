15d2478c4b8e7941efbe2b7f2d8c798f5d3df27c
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2022-06-16 21:13:05 +0700

    feat: add usecase to register user

--- backend/usecases/users.go
+++ backend/usecases/users.go
@@ -1,19 +1,49 @@
 package usecases
 
 import (
+	"fmt"
+
+	"github.com/rg-km/final-project-engineering-16/backend/app/middleware"
+	"github.com/rg-km/final-project-engineering-16/backend/commons/exceptions"
 	"github.com/rg-km/final-project-engineering-16/backend/domains"
 )
 
-type UserUsecase struct {
+type AuthUsecase struct {
 	Repo domains.UserRepository
+	TokenAuth middleware.JWTService
 }
 
-func NewUserUsecase(repo domains.UserRepository) domains.UserUsecase {
-	return &UserUsecase{
-		Repo:           repo,
+func NewAuthUsecase(repo domains.UserRepository, tokenAuth middleware.JWTService) AuthUsecase {
+	return AuthUsecase{
+		Repo: repo,
+		TokenAuth: tokenAuth,
 	}
 }
 
-func (u *UserUsecase) Login(user domains.User) (domains.User, error) {
-	return u.Repo.Login(user.Email, user.Password)
+func (a AuthUsecase) Login(user domains.User) (domains.User, error) {
+	fmt.Printf("user:\n%+v\n", user)
+	user, err := a.Repo.Login(user.Email, user.Password)
+	if err != nil {
+		return user, err
+	}
+	if user.Role == "admin" {
+		user.Token = a.TokenAuth.GenerateToken(user.Email, true, false)
+	} else {
+		user.Token = a.TokenAuth.GenerateToken(user.Email, false, false)
+	}
+	return user, nil
+}
+
+func (a AuthUsecase) Register(user domains.CreateUser) (domains.User, error) {
+	isEmailExist := a.Repo.CheckAccountEmail(user.Email)
+	if isEmailExist {
+		fmt.Printf("%s\nEmail %+v is already exist\n", user.Email, isEmailExist)
+		return domains.User{}, exceptions.ErrUserAlreadyExists
+	}
+
+	createdUser, err := a.Repo.Create(user.Fullname, user.Email, user.Password, user.Address, user.PhoneNumber, 1)
+	if err != nil {
+		return createdUser, err
+	}
+	return createdUser, nil
 }
