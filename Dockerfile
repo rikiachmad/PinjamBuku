c0e3f2a2885ac1b3cfb94ea5d7b2958cd0bd60ed
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2022-06-17 22:18:02 +0700

    feat: add libraries table migration

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM golang:1.17.5-alpine AS builder
 
+RUN apk add build-base
+
 RUN mkdir /app
 ADD . /app
 WORKDIR /app
@@ -11,7 +13,7 @@ FROM alpine:3.6
 
 WORKDIR /root/
 
-COPY --from=builder /app/.env .
+COPY --from=builder /app/backend/.env .
 COPY --from=builder /app/main .
 
 EXPOSE 8080
--- backend/infrastructures/database/migration/main.go
+++ backend/infrastructures/database/migration/main.go
@@ -47,8 +47,24 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
+	
 	_, err = db.Exec(`INSERT INTO user_roles (name) VALUES ("ADMIN"), ("USER");`)
+	
+	if err != nil {
+		panic(err)
+	}
+	
+	_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS libraries (
+			id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+			name VARCHAR(255) NOT NULL,
+			address VARCHAR(255) NOT NULL,
+			phone_number VARCHAR(255) NOT NULL,
+			verified_date DATETIME,
+			created_at DATETIME,
+			updated_at DATETIME
+		);
+	`)
 
 	if err != nil {
 		panic(err)
@@ -63,6 +79,12 @@ func Rollback(db *sql.DB) {
 	}
 
 	sqlStmt = `DROP TABLE user_roles;`
+	_, err = db.Exec(sqlStmt)
+	if err != nil {
+		panic(err)
+	}
+
+	sqlStmt = `DROP TABLE libraries;`
 	_, err = db.Exec(sqlStmt)
 	if err != nil {
 		panic(err)
