69a15feae9fad8081fc402d1ac5a0c2fae84d916
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2022-06-19 17:14:07 +0700

    feat: add migration for book_categories, books, and carts tables

--- backend/infrastructures/database/migration/main.go
+++ backend/infrastructures/database/migration/main.go
@@ -9,7 +9,6 @@ import (
 // Run This Script for migration db
 func main() {
 	db, err := sql.Open("sqlite3", "./pinjambuku.db")
-
 	if err != nil {
 		panic(err)
 	}
@@ -32,7 +31,6 @@ func main() {
 			FOREIGN KEY(role_id) REFERENCES user_roles(id)
         );
     `)
-
 	if err != nil {
 		panic(err)
 	}
@@ -43,13 +41,11 @@ func main() {
 			name VARCHAR(10) NOT NULL
         );
     `)
-
 	if err != nil {
 		panic(err)
 	}
 	
 	_, err = db.Exec(`INSERT INTO user_roles (name) VALUES ("ADMIN"), ("USER");`)
-	
 	if err != nil {
 		panic(err)
 	}
@@ -60,12 +56,60 @@ func main() {
 			name VARCHAR(255) NOT NULL,
 			address VARCHAR(255) NOT NULL,
 			phone_number VARCHAR(255) NOT NULL,
-			verified_date DATETIME,
 			created_at DATETIME,
 			updated_at DATETIME
 		);
 	`)
+	if err != nil {
+		panic(err)
+	}
 
+	_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS book_categories (
+			id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+			name VARCHAR(255) NOT NULL,
+			created_at DATETIME,
+			updated_at DATETIME
+		);
+	`)
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS books (
+			id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+			katalog_id VARCHAR(10) NOT NULL,
+			library_id INTEGER NOT NULL,
+			category_id INTEGER NOT NULL,
+			title VARCHAR(512) NOT NULL,
+			author VARCHAR(255) NOT NULL,
+			page_number INT NOT NULL,
+			stock INT NOT NULL,
+			description TEXT NOT NULL,
+			deposit BIGINT NOT NULL,
+			cover VARCHAR(1024) NOT NULL,
+			created_at DATETIME,
+			updated_at DATETIME,
+			FOREIGN KEY(library_id) REFERENCES libraries(id),
+			FOREIGN KEY(category_id) REFERENCES book_categories(id)
+		);
+	`)
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS carts (
+			id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+			user_id INTEGER NOT NULL,
+			book_id INTEGER NOT NULL,
+			created_at DATETIME,
+			updated_at DATETIME,
+			FOREIGN KEY(user_id) REFERENCES users(id),
+			FOREIGN KEY(book_id) REFERENCES books(id)
+		);
+	`)
 	if err != nil {
 		panic(err)
 	}
@@ -85,6 +129,24 @@ func Rollback(db *sql.DB) {
 	}
 
 	sqlStmt = `DROP TABLE libraries;`
+	_, err = db.Exec(sqlStmt)
+	if err != nil {
+		panic(err)
+	}
+
+	sqlStmt = `DROP TABLE book_categories;`
+	_, err = db.Exec(sqlStmt)
+	if err != nil {
+		panic(err)
+	}
+
+	sqlStmt = `DROP TABLE books;`
+	_, err = db.Exec(sqlStmt)
+	if err != nil {
+		panic(err)
+	}
+
+	sqlStmt = `DROP TABLE carts;`
 	_, err = db.Exec(sqlStmt)
 	if err != nil {
 		panic(err)
