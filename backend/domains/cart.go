c60606385b3d2ebae2c5fd388489dd711ea7f064
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2022-06-19 21:25:47 +0700

    feat: add domain layer and interfaces for book

---
+++ backend/domains/cart.go
@@ -0,0 +1,21 @@
+package domains
+
+type Cart struct {
+	ID        int64  `db:"id"`
+	UserID    int64  `db:"user_id"`
+	BookID    int64  `db:"book_id"`
+	User      User   `db:"user"`
+	Book      Book   `db:"book"`
+	CreatedAt string `db:"created_at"`
+	DeletedAt string `db:"deleted_at"`
+}
+
+type CartRepository interface {
+	FetchCartByID(id int64) (Cart, error)
+	FetchCartByUserID(userID int64) ([]Cart, error)
+	InsertToCart(userID, bookID int64) (Cart, error)
+}
+
+type CartUsecase interface {
+	ShowCartByUserID(id int64) ([]Cart, error)
+}
