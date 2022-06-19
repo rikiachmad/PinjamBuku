c8b46128b9e1ded53a042cfe12f245ad9377657e
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2022-06-19 21:30:45 +0700

    feat: add repository layer for cart

---
+++ backend/infrastructures/repository/carts.go
@@ -0,0 +1,112 @@
+package repository
+
+import (
+	"database/sql"
+
+	domains "github.com/rg-km/final-project-engineering-16/backend/domains"
+)
+
+type CartRepository struct {
+	db *sql.DB
+}
+
+func NewCartRepository(db *sql.DB) domains.CartRepository {
+	return &CartRepository{db: db}
+}
+
+func (u *CartRepository) FetchCartByID(id int64) (domains.Cart, error) {
+	sqlStmt := `SELECT c.id, u.id, u.fullname, u.address, u.email, u.phone_number, u.verified_date, 
+	b.id, b.library_id, b.title, b.author, b.page_number, b.deposit, b.cover
+	FROM carts c 
+	INNER JOIN users u ON c.user_id = u.id
+	INNER JOIN books b ON c.book_id = b.id
+	WHERE c.id = ?`
+
+	cart := domains.Cart{}
+
+	row := u.db.QueryRow(sqlStmt, id)
+	err := row.Scan(
+		&cart.ID,
+		&cart.User.ID,
+		&cart.User.Fullname,
+		&cart.User.Address,
+		&cart.User.Email,
+		&cart.User.PhoneNumber,
+		&cart.User.Verified,
+		&cart.Book.ID,
+		&cart.Book.LibraryID,
+		&cart.Book.Title,
+		&cart.Book.Author,
+		&cart.Book.PageNumber,
+		&cart.Book.Deposit,
+		&cart.Book.CoverPict,
+	)
+
+	if err != nil {
+		return domains.Cart{}, err
+	}
+
+	return cart, nil
+}
+
+func (u *CartRepository) FetchCartByUserID(userID int64) ([]domains.Cart, error) {
+	sqlStmt := `SELECT c.id, u.id, u.fullname, u.address, u.email, u.phone_number, u.verified_date, 
+	b.id, b.library_id, b.title, b.author, b.page_number, b.deposit, b.cover
+	FROM carts c 
+	INNER JOIN users u ON c.user_id = u.id
+	INNER JOIN books b ON c.book_id = b.id
+	WHERE c.user_id = ?`
+
+	carts := []domains.Cart{}
+
+	rows, err := u.db.Query(sqlStmt, userID)
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		cart := domains.Cart{}
+
+		err := rows.Scan(
+			&cart.ID,
+			&cart.User.ID,
+			&cart.User.Fullname,
+			&cart.User.Address,
+			&cart.User.Email,
+			&cart.User.PhoneNumber,
+			&cart.User.Verified,
+			&cart.Book.ID,
+			&cart.Book.LibraryID,
+			&cart.Book.Title,
+			&cart.Book.Author,
+			&cart.Book.PageNumber,
+			&cart.Book.Deposit,
+			&cart.Book.CoverPict,
+		)
+
+		if err != nil {
+			return nil, err
+		}
+
+		carts = append(carts, cart)
+	}
+
+	return carts, nil
+}
+
+func (u *CartRepository) InsertToCart(userID int64, bookID int64) (domains.Cart, error) {
+	sqlStmt := `INSERT INTO carts (user_id, book_id) VALUES (?, ?) RETURNING id, user_id, book_id`
+
+	cart := domains.Cart{}
+
+	err := u.db.QueryRow(sqlStmt, userID, bookID).Scan(
+		&cart.ID,
+		&cart.User.ID,
+		&cart.Book.ID,
+	)
+	if err != nil {
+		return domains.Cart{}, err
+	}
+
+	return cart, nil
+}
