f4ca8d4720f2f3a62bbdb8f362ae26d01e738358
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2022-06-21 22:32:27 +0700

    feat: add some repository to cart

--- backend/infrastructures/repository/carts.go
+++ backend/infrastructures/repository/carts.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"database/sql"
 
+	"github.com/rg-km/final-project-engineering-16/backend/commons/exceptions"
 	domains "github.com/rg-km/final-project-engineering-16/backend/domains"
 )
 
@@ -42,6 +43,28 @@ func (u *CartRepository) FetchCartByID(id int64) (domains.Cart, error) {
 	)
 
 	if err != nil {
+		return domains.Cart{}, err
+	}
+
+	return cart, nil
+}
+
+func (u *CartRepository) CheckCartByUserIDAndBookID(userID int64, bookID int64) (domains.Cart, error) {
+	sqlStmt := `SELECT id, user_id, book_id FROM carts WHERE user_id = ? AND book_id = ?`
+
+	cart := domains.Cart{}
+
+	row := u.db.QueryRow(sqlStmt, userID, bookID)
+	err := row.Scan(
+		&cart.ID,
+		&cart.UserID,
+		&cart.BookID,
+	)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return domains.Cart{}, exceptions.ErrCartNotFound
+		}
 		return domains.Cart{}, err
 	}
 
@@ -99,12 +122,34 @@ func (u *CartRepository) InsertToCart(userID int64, bookID int64) (domains.Cart,
 
 	err := u.db.QueryRow(sqlStmt, userID, bookID).Scan(
 		&cart.ID,
-		&cart.User.ID,
-		&cart.Book.ID,
+		&cart.UserID,
+		&cart.BookID,
 	)
 	if err != nil {
 		return domains.Cart{}, err
 	}
 
 	return cart, nil
+}
+
+func (u *CartRepository) DeleteCartByID(id int64) error {
+	sqlStmt := `DELETE FROM carts WHERE id = ?`
+
+	_, err := u.db.Exec(sqlStmt, id)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (u *CartRepository) DeleteCartByUserID(userID int64) error {
+	sqlStmt := `DELETE FROM carts WHERE user_id = ?`
+
+	_, err := u.db.Exec(sqlStmt, userID)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
