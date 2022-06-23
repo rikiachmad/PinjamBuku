89df748b4c4b8bbd640163e3003d76304398d637
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2022-06-23 21:36:26 +0700

    feat: add domain struct for borrowing entity

---
+++ backend/domains/borrowing.go
@@ -0,0 +1,36 @@
+package domains
+
+type BorrowingStatus struct {
+	ID     int64  `json:"id"`
+	Status string `json:"status"`
+}
+
+type Borrowing struct {
+	ID            int64           `db:"id"`
+	UserID        int64           `db:"user_id"`
+	StatusID      int64           `db:"status_id"`
+	User          User            `db:"user"`
+	Book          Book            `db:"book"`
+	Library       Library         `db:"library"`
+	TotalDeposit  int64           `db:"total_deposit"`
+	TotalCost     int64           `db:"total_cost"`
+	BorrowingDate string          `db:"borrowing_date"`
+	DueDate       string          `db:"due_date"`
+	FinishDate    string          `db:"finish_date"`
+	Status        BorrowingStatus `db:"status"`
+	CreatedAt     string          `db:"created_at"`
+	DeletedAt     string          `db:"deleted_at"`
+}
+
+type BorrowingRepository interface {
+	FetchBorrowingByID(id int64) (Borrowing, error)
+	FetchBorrowingByUserID(userID int64) ([]Borrowing, error)
+	InsertToBorrowing(userID int64, bookID []int64, totalDeposit int64, totalCost int64) error
+	DeleteBorrowingByID(id int64) error
+}
+
+type BorrowingUsecase interface {
+	ShowBorrowingByUserID(id int64) ([]Borrowing, error)
+	InsertToBorrowing(userID, bookID int64) (Borrowing, error)
+	DeleteBorrowingByID(id int64) error
+}
